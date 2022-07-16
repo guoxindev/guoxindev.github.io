891f3af312047f3592090364e82d4b131e8d11b4
Chan Guo Xin <yt3xgxuox@outlook.my>
2022-07-16 20:03:33 +0800

    created

---
+++ req/blog-main-proccess.js
@@ -0,0 +1,11 @@
+setTimeout(() => {
+    console.clear()
+}, 5000);
+function main(call,ext1){
+    if (call=="rc"){
+        window.location.href=go
+    }
+    if(call=="404"){
+        document.getElementById("404").innerHTML="Location "+"["+location.pathname+"]"+"is not found on the server"
+    }
+}
